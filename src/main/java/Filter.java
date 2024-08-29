e88dd28226fc830a0cb38dcdc12c01d0d099330f
Mageshkumar M <sumeshattec@gmail.com>
2024-08-29 22:51:32 +0530

    Filter

---
+++ src/main/java/Filter.java
@@ -0,0 +1,10 @@
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Filter {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Filter00");
+    }
+
+}
